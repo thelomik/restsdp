df5cf6d5cb4abff2436541912b2a7084f3721f09
bartosz.baranski <bartosz.marek.baranski@gmail.com>
2021-01-07 00:59:08 +0100

    Added swagger documentation.

--- registry/src/main/java/com/globallogic/vehicle/registry/controller/RegistryController.java
+++ registry/src/main/java/com/globallogic/vehicle/registry/controller/RegistryController.java
@@ -1,5 +1,9 @@
 package com.globallogic.vehicle.registry.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/registry")
 @Slf4j
+@Api(description = "Article Management API")
 public class RegistryController {
 
+    @ApiOperation(value = "Returns a specified entity.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Entity found") })
     @GetMapping(path = "/{vin}", produces = MediaType.APPLICATION_JSON_VALUE)
     public VehicleSO get(@PathVariable(name = "vin") String vin) {
         VehicleSO vehicleSO = new VehicleSO();
