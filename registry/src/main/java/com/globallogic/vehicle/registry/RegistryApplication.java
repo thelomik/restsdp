43a34c17aa344bedc7a327471317034ba7db5346
bartosz.baranski <bartosz.marek.baranski@gmail.com>
2021-01-07 15:09:01 +0100

    Added model mapper.

--- registry/src/main/java/com/globallogic/vehicle/registry/RegistryApplication.java
+++ registry/src/main/java/com/globallogic/vehicle/registry/RegistryApplication.java
@@ -1,7 +1,10 @@
 package com.globallogic.vehicle.registry;
 
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RegistryApplication {
@@ -10,4 +13,10 @@ public class RegistryApplication {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+		return modelMapper;
+	}
 }
--- registry/src/main/java/com/globallogic/vehicle/registry/service/RegistryService.java
+++ registry/src/main/java/com/globallogic/vehicle/registry/service/RegistryService.java
@@ -4,6 +4,7 @@ import com.globallogic.vehicle.registry.controller.VehicleSO;
 import com.globallogic.vehicle.registry.entities.Vehicle;
 import com.globallogic.vehicle.registry.exceptions.RegistryResourceNotFound;
 import com.globallogic.vehicle.registry.repository.RegistryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,9 @@ public class RegistryService {
     @Autowired
     private RegistryRepository registryRepository;
 
+    @Autowired
+    protected ModelMapper modelMapper;
+
     public VehicleSO get(String vin) {
         Vehicle found = registryRepository.findByVin(vin);
 
@@ -20,30 +24,32 @@ public class RegistryService {
             throw new RegistryResourceNotFound("Vehicle with given VIN does not exist.");
         }
 
-        VehicleSO vehicleSO = new VehicleSO();
-        vehicleSO.setVin(found.getVin());
-        vehicleSO.setProductionYear(found.getProductionYear());
-        vehicleSO.setModel(found.getModel());
-        vehicleSO.setBrand(found.getBrand());
+//        VehicleSO vehicleSO = new VehicleSO();
+//        vehicleSO.setVin(found.getVin());
+//        vehicleSO.setProductionYear(found.getProductionYear());
+//        vehicleSO.setModel(found.getModel());
+//        vehicleSO.setBrand(found.getBrand());
 
-        return vehicleSO;
+        return modelMapper.map(found, VehicleSO.class);
     }
 
     public VehicleSO create(VehicleSO so) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setBrand(so.getBrand());
-        vehicle.setModel(so.getModel());
-        vehicle.setProductionYear(so.getProductionYear());
-        vehicle.setVin(so.getVin());
+        Vehicle vehicle = modelMapper.map(so, Vehicle.class);
+//        Vehicle vehicle = new Vehicle();
+//        vehicle.setBrand(so.getBrand());
+//        vehicle.setModel(so.getModel());
+//        vehicle.setProductionYear(so.getProductionYear());
+//        vehicle.setVin(so.getVin());
 
         Vehicle save = registryRepository.save(vehicle);
-        VehicleSO created = new VehicleSO();
-        created.setBrand(save.getBrand());
-        created.setModel(save.getModel());
-        created.setProductionYear(save.getProductionYear());
-        created.setId(save.getId());
-        created.setVin(save.getVin());
 
-        return created;
+//        VehicleSO created = new VehicleSO();
+//        created.setBrand(save.getBrand());
+//        created.setModel(save.getModel());
+//        created.setProductionYear(save.getProductionYear());
+//        created.setId(save.getId());
+//        created.setVin(save.getVin());
+
+        return modelMapper.map(save, VehicleSO.class);
     }
 }
